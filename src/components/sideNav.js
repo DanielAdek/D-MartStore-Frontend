2d61126d386dd5b0a01969076b883ddcc74958bb
olorunwa lawrence <olorunwalawrence5@gmail.com>
2019-12-21 11:37:49 -0800

    ft: create logout functionality

--- src/components/sideNav.js
+++ src/components/sideNav.js
@@ -1,28 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 // import { Link } from 'react-router-dom';
 import * as Nav from '../assets/styles/nav-p';
+import { useHistory } from 'react-router-dom';
 
 export const SideBarNav = props => {
-  return (
-    <Nav.SideBarContainer>
-      <Nav.SideBarHeading>Navigation</Nav.SideBarHeading>
-      <Nav.SideBarItems>
-        <Nav.SideBarItem>
-          <Nav.SideBarSpan onClick={() => props.clicked(1)}>Dashboard</Nav.SideBarSpan>
-        </Nav.SideBarItem>
-        <Nav.SideBarItem>
-          <Nav.SideBarSpan onClick={() => props.clicked(2)}>Edit Profile</Nav.SideBarSpan>
-        </Nav.SideBarItem>
-        <Nav.SideBarItem>
-          <Nav.SideBarSpan onClick={() => props.clicked(3)}>Create Product</Nav.SideBarSpan>
-        </Nav.SideBarItem>
-        <Nav.SideBarItem>
-          <Nav.SideBarSpan onClick={() => props.clicked(4)}>Order History</Nav.SideBarSpan>
-        </Nav.SideBarItem>
-        <Nav.SideBarItem>
-          <Nav.SideBarSpan onClick={() => props.clicked(5)}>Logout</Nav.SideBarSpan>
-        </Nav.SideBarItem>
-      </Nav.SideBarItems>
-    </Nav.SideBarContainer>
-  )
-}
+	const [logout, setLogout] = useState(false);
+
+	const history = useHistory();
+	const signOut = e => {
+		e.preventDefault();
+		localStorage.clear('token');
+		setLogout(true);
+		history.push('/');
+	};
+
+	return (
+		<Nav.SideBarContainer>
+			<Nav.SideBarHeading>Navigation</Nav.SideBarHeading>
+			<Nav.SideBarItems>
+				<Nav.SideBarItem>
+					<Nav.SideBarSpan onClick={() => props.clicked(1)}>Dashboard</Nav.SideBarSpan>
+				</Nav.SideBarItem>
+				<Nav.SideBarItem>
+					<Nav.SideBarSpan onClick={() => props.clicked(2)}>Edit Profile</Nav.SideBarSpan>
+				</Nav.SideBarItem>
+				<Nav.SideBarItem>
+					<Nav.SideBarSpan onClick={() => props.clicked(3)}>Create Product</Nav.SideBarSpan>
+				</Nav.SideBarItem>
+				<Nav.SideBarItem>
+					<Nav.SideBarSpan onClick={() => props.clicked(4)}>Order History</Nav.SideBarSpan>
+				</Nav.SideBarItem>
+				<Nav.SideBarItem>{<Nav.SideBarSpan onClick={signOut}>Logout</Nav.SideBarSpan>}</Nav.SideBarItem>
+			</Nav.SideBarItems>
+		</Nav.SideBarContainer>
+	);
+};
